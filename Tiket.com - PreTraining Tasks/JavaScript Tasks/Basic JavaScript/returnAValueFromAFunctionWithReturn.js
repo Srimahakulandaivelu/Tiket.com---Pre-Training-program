f1301462feb4bf5a32e7d2dcd076dec1da3f90c1
Mahaa <srimahakulandaivelu05@gmail.com>
2026-07-27 21:42:42 +0530

    Basic JS exercises

---
+++ Tiket.com - PreTraining Tasks/JavaScript Tasks/Basic JavaScript/returnAValueFromAFunctionWithReturn.js
@@ -0,0 +1,7 @@
+function timesFive(param1){
+  return param1 * 5;
+}
+
+console.log(timesFive(5));
+console.log(timesFive(2));
+console.log(timesFive(0));
---
+++ Tiket.com - PreTraining Tasks/JavaScript Tasks/Basic JavaScript/shoppingList.js
@@ -0,0 +1,7 @@
+const myList = [];
+
+myList.push(["Soap", 5]);
+myList.push(["Biscuit", 10]);
+myList.push(["shampoo", 2]);
+myList.push(["Rice", 3]);
+myList.push(["Chilli", 56]);
